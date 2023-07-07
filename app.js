4ef9a7c401d58d3b89164b21ff63d76ddb6b1067
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-07-07 14:40:00 +0530

    Added Level 2 Auth

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const userSchema = mongoose.Schema({
     email: String,
     password: String,
 });
-
+//mongoose-encryption
 userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
