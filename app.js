bcb22ec8c5a9547d777784754e2b9c72741a9b5c
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-07-09 00:24:52 +0530

    Implemented Google Auth 2.0

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const session = require("express-session");
 const LocalStrategy = require("passport-local");
 const passportLocalMongoose = require("passport-local-mongoose");
 const path = require("node:path");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 require("dotenv").config();
 
 const app = express();
@@ -48,7 +50,7 @@ mongoose.connect(local_URI, {
 const userSchema = mongoose.Schema({
     email: String,
     password: String,
-    googleID: String,
+    googleId: String,
     secret: String,
 });
 //mongoose-encryption
@@ -59,18 +61,67 @@ const userSchema = mongoose.Schema({
 
 //passport-local-mongoose plugin
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 // CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, {
+            id: user.id,
+            username: user.username,
+            picture: user.picture,
+        });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/google/secrets",
+            // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+            // userProfileURL: "https://www.googleapis.com/auth/profile.emails.read",
+        },
+        function (accessToken, refreshToken, profile, cb) {
+            console.log(profile);
+            User.findOrCreate({ googleId: profile.id }, function (err, user) {
+                return cb(err, user);
+            });
+        }
+    )
+);
 
 app.get("/", function (req, res) {
     res.render("home");
 });
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+    "/auth/google/secrets",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function (req, res) {
+        // Successful authentication, redirect to secrets.
+        res.redirect("/secrets");
+    }
+);
 
 app.get("/login", function (req, res) {
     res.render("login");
@@ -87,7 +138,7 @@ app.get("/secrets", async function (req, res) {
     //     res.redirect("/login");
     // }
     try {
-        const users = await User.find({ secret: { $ne: null } });
+        const users = await User.find({ "secret": { $ne: null } });
         if (users) {
             res.render("secrets", { usersWithSecrets: users });
         } else {
